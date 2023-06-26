dd0165880052b98bffc1cdb7923aa6b5e72af7e8
Eduardo <eduardo.buchele@gmail.com>
2023-06-26 14:41:44 -0300

    Add tests useCase category TDD

--- application/src/test/java/com/admin/catalogo/application/UseCaseTest.java
+++ application/src/test/java/com/admin/catalogo/application/UseCaseTest.java
@@ -1,13 +1,10 @@
 package com.admin.catalogo.application;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UseCaseTest {
 
-    @Test
-    public void testCreateUseCase() {
-        Assertions.assertNotNull(new UseCase());
-        Assertions.assertNotNull(new UseCase().execute());
-    }
 }
