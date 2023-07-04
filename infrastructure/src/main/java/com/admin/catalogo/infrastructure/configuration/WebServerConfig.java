406493b9d5bba778f47c1199f859ab8205be654b
Eduardo <eduardo.buchele@gmail.com>
2023-07-04 15:15:37 -0300

    Add spring and mysql

---
+++ infrastructure/src/main/java/com/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.admin.catalogo")
+public class WebServerConfig {
+}
